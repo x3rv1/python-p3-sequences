f102e6ec92ffeb02d20b442b760e8cd42455f2ba
Peter Xervi <xervipeter@gmail.com>
2025-11-21 10:50:51 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    """
+    Prints a list of the Fibonacci sequence up to length n.
+    
+    Parameters:
+    n (int): The number of terms to generate in the sequence.
+    """
+    if n <= 0:
+        print([])  # No terms if n is 0 or negative
+        return
+    
+    # Start with the first two Fibonacci numbers
+    sequence = [0, 1]
+    
+    # Generate the sequence until it reaches length n
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    
+    # Trim in case n = 1
+    print(sequence[:n])
